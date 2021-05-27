623abef0ae1ff282f1fee21da99d4a7349693e78
williamsjose <moflin02@gmail.com>
2021-05-26 23:42:22 -0300

    eureka server criado

--- eureka-service/src/main/java/br/com/ms/EurekaServiceApplication.java
+++ eureka-service/src/main/java/br/com/ms/EurekaServiceApplication.java
@@ -2,14 +2,14 @@ package br.com.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class MsEmpregadoApplication {
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsEmpregadoApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
---
+++ ms-empregado/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO TB_EMPREGADOS (nome, salario) VALUES ('Williams Silva', 200.0);
+INSERT INTO TB_EMPREGADOS (nome, salario) VALUES ('Paulo Serio', 50.0);
+INSERT INTO TB_EMPREGADOS (nome, salario) VALUES ('Nunes Mairo', 1000.0);
+INSERT INTO TB_EMPREGADOS (nome, salario) VALUES ('Danylo Sylva', 12000.0);
+INSERT INTO TB_EMPREGADOS (nome, salario) VALUES ('Victor Montano', 600.0);
