a0d73dacdfebb27daf55723b420e23c47fac08f2
williamsjose <moflin02@gmail.com>
2021-05-26 21:45:03 -0300

    iniciando projeto

---
+++ ms-empregado/src/main/java/br/com/ms/MsEmpregadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmpregadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmpregadoApplication.class, args);
+	}
+
+}
